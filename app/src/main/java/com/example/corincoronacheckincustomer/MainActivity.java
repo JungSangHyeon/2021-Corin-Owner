b9d539fc888a38ad8e26317f44a33a5cb89c429f
JungSangHyeon <solea1204@naver.com>
2021-06-05 19:01:25 +0900

    UI - Framework & Corona Info

--- app/src/main/java/com/example/corincoronacheckincustomer/MainActivity.java
+++ app/src/main/java/com/example/corincoronacheckincustomer/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.corincoronacheckincustomer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = this.findViewById(R.id.mainFragment_bottomNavigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+            switch(item.getItemId()){
+                case R.id.action_home: navController.navigate(R.id.action_global_coronaInfoFragment); break;
+                case R.id.action_check_in: navController.navigate(R.id.action_global_checkInFragment); break;
+                case R.id.action_my_corin: navController.navigate(R.id.action_global_myCorinFragment); break;
+            }
+            return true;
+        });
     }
 }
